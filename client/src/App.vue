e3a33fb4ce1635c8940fe6feb767490ae1edbd21
yennypedrazaza <yenny.pedraza@gmail.com>
2021-09-26 13:50:04 -0500

    Agregando carpeta Client

---
+++ client/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-app>
+    <Header/>
+    
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
+<!-- /*<div id="nav">
+  <router-link  to="/">Home</router-link>|
+  <router-link to="/about">About</router-link>|
+  <router-link to="/nosotros">Nosotros</router-link>|
+  <router-link to="/section">Section</router-link>|
+  <router-link to="/pedido">Pedido</router-link>|
+</div>/ -->
+</template>
+
+<script>
+import Header from './components/Header.vue'
+
+export default {
+  name: 'App',
+  components: {
+      Header,
+      
+    },
+
+  data: () => ({
+    //
+  }),
+};
+
+</script>
---
+++ client/src/views/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  
+    <Section/>
+    
+
+</template>
+
+<script>
+  import Section from './Section.vue'
+  
+  export default {
+    name: 'Home',
+
+    components: {
+      Section,
+      
+
+    },
+  }
+</script>
