fcfd41a3e6a31f8a9c0af2b3b2e0dd921120b862
yennypedrazaza <yenny.pedraza@gmail.com>
2021-09-28 21:08:30 -0500

    En app.vue se enrutaron las vistas

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,18 +1,18 @@
 <template>
-  <v-app>
+  <!--/*<v-app>
     <Header/>
     
     <v-main>
       <router-view/>
     </v-main>
-  </v-app>
-<!-- /*<div id="nav">
+  </v-app>/ -->
+<div id="nav">
   <router-link  to="/">Home</router-link>|
   <router-link to="/about">About</router-link>|
   <router-link to="/nosotros">Nosotros</router-link>|
   <router-link to="/section">Section</router-link>|
   <router-link to="/pedido">Pedido</router-link>|
-</div>/ -->
+</div>
 </template>
 
 <script>
